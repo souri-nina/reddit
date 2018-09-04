c8e7e88599a3725f5305382da3738d96147c4639
souri.nina@gmail.com <snmoukdahan@gmail.com>
2018-09-04 10:27:39 -0600

    sub model, contoller & routes generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root "subs#index"
+
+  resources :subs
+
+  get "/subs", to: "subs#index"
+  get "/subs/:id", to: "subs#show"
+  delete "/subs/:id", to: "subs#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
